104b07eac1db46b2b7b2c4a83adf5a048ad4bd93
Pradeep Rawat <115552630+rishPR@users.noreply.github.com>
2024-01-08 20:40:43 +0530

    Update Dice Combinatins.cpp

--- Dynamic Programming/Dice Combinatins.cpp
+++ Dynamic Programming/Dice Combinatins.cpp
@@ -1,15 +1,9 @@
 //TABULATION SOLUTION - BOTTOM UP APPROACH
 #include<bits/stdc++.h>
-
 using namespace std;
-
-
-
- 
 void solve() {
   ll n;
   cin>>n;
- 
   vector<ll>dp(n+1,0);
   dp[0] = 1;
   for(int i=1;i<=n;i++){
@@ -20,10 +14,7 @@ void solve() {
   }
   cout<<dp[n]<<endl;
 }
- 
- 
 int main() {
-
     solve();
     return 0;
 }
